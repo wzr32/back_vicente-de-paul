867414164c3e326ffdd7593287cee2eee1e71b56
Ruben Padilla <alejandro010492@gmail.com>
2023-08-15 11:33:29 -0400

    sync

--- src/database/database.ts
+++ src/database/database.ts
@@ -14,11 +14,11 @@ import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from "../config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: DB_HOST || "dpg-cjdhulgq339s73alsnfg-a.oregon-postgres.render.com",
+  host: DB_HOST || "localhost",
   port: Number(DB_PORT) || 5432,
   username: DB_USER || "root",
-  password: DB_PASS || "pcdd1qFWxYuj8juqmejDtXTkgPNlirW0",
-  database: DB_NAME || "school_7mh5",
+  password: DB_PASS || "pass",
+  database: DB_NAME || "school",
   synchronize: true,
   logging: false,
   entities: [
