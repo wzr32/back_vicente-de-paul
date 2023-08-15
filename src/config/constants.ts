b8d5ef0a985b5faa692c3b79cf835b27c4f6ec22
Ruben Padilla <alejandro010492@gmail.com>
2023-08-15 03:12:10 -0400

    sync

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,9 @@
 import { Secret } from "jsonwebtoken";
 
 export const SECRET_KEY = process.env.SECRET_KEY as Secret;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_NAME = process.env.DB_NAME;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASS = process.env.DB_PASS;
 export const SALT_ROUNDS = 15;
