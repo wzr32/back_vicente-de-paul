3b7119eca5b3751137e7ed21026a8d9018f6d8ea
Ruben Padilla <alejandro010492@gmail.com>
2023-08-15 13:00:20 -0400

    fix

--- src/database/database.ts
+++ src/database/database.ts
@@ -14,11 +14,11 @@ import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from "../config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: DB_HOST || "localhost",
-  port: Number(DB_PORT) || 5432,
-  username: DB_USER || "root",
-  password: DB_PASS || "pass",
-  database: DB_NAME || "school",
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
   synchronize: true,
   logging: false,
   entities: [
