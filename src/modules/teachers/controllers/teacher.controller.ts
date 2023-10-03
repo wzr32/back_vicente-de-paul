1af5ef9fe7aeafd5fdd6f63adf2b1d96c5c37d8f
Ruben Padilla <alejandro010492@gmail.com>
2023-10-03 03:42:20 -0400

    sync

--- src/modules/teachers/controllers/teacher.controller.ts
+++ src/modules/teachers/controllers/teacher.controller.ts
@@ -92,14 +92,3 @@ export const getTeacherGroups = async (
     console.log("error getting student by dni", error);
   }
 };
-
-export const getUpdateStudentsGrades = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-  } catch (error) {
-    res.status(400).json({ error: "Error obteniendo estudiante" });
-    console.log("error getting student by dni", error);
-  }
-};
